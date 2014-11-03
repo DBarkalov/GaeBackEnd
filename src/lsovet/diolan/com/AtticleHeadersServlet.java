69c0a4d11a0961405333e003de7c726447a9296c
Dmitry Barkalov <d.barkalov@corp.mail.ru>
2014-11-03 18:35:25 +0300

    refactor, add ttl

--- src/lsovet/diolan/com/AtticleHeadersServlet.java
+++ src/lsovet/diolan/com/AtticleHeadersServlet.java
@@ -1,4 +1,5 @@
 package lsovet.diolan.com;
+
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -6,59 +7,126 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.*;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Text;
+import com.google.appengine.api.users.User;
+
 @SuppressWarnings("serial")
 public class AtticleHeadersServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		String page = req.getParameter("page");
-		String url = "http://lsovet.ru/" + (page == null ? "": "page/"+page+"/");
-		
-		try {
-		    URL myURL = new URL(url);
-		    URLConnection con = myURL.openConnection();
-		    con.setRequestProperty("Accept-Charset", "UTF-8");
-		    con.connect();
-		    con.setConnectTimeout(10*1000);
-		    byte[] responce = readByParts(con.getInputStream());		    
-		    String str = new String(responce, "UTF-8");
-		  
-		    ArticleHeaderParser parser = new ArticleHeaderParser();
-		    List<ArticleHeader> a = parser.parse(str);
-		  
-		    resp.setContentType("text/plain");
-		    resp.setCharacterEncoding("UTF-8");
-		    
-		    resp.getWriter().write("[");
-		    
-			if (a.size() > 0) {
-				if (a.size() > 1) {
-					for (int i = 0; i < a.size() - 1; i++) {
-						resp.getWriter().write(a.get(i).toString());
-						resp.getWriter().println(",");
-					}
-				}
-				resp.getWriter().write(a.get(a.size() - 1).toString());
-				resp.getWriter().write("]");
+	private static final String ATTICLE_HEADERS = "AtticleHeaders";
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		final String page = req.getParameter("page");
+		final String url = "http://lsovet.ru/"	+ (page == null ? "" : "page/" + page + "/");
+		Key headerKey = KeyFactory.createKey(ATTICLE_HEADERS, url);
+
+		String content = getContentFromCache(headerKey);
+		if (content != null) {
+			setOkResponce(resp, content);
+		} else {
+			byte[] responceData = null;
+			try {
+				responceData = getContentFromNet(url);
+			} catch (IOException e) {
+				setErrorResponce(resp, 400, "fetch url error");
 			}
-		  
-		    
-		} 
-		catch (MalformedURLException e) { 
-			resp.setStatus(400);
-			resp.getWriter().println(e.getMessage());
-		} 
-		catch (IOException e) {   
-			resp.setStatus(400);
-			resp.getWriter().println(e.getMessage());
+			if (responceData != null) {
+				ArticleHeaderParser parser = new ArticleHeaderParser();
+				final List<ArticleHeader> articles = parser.parse(new String(responceData, "UTF-8"));
+				if (articles.size() > 0) {
+					content = createContent(articles);
+					setOkResponce(resp, content);
+					saveContentToCache(headerKey, content);
+				} else {
+					setErrorResponce(resp, 400, "parse data error");
+				}
+			}
 		}
-		
+
+	}
+
+
+	private void setOkResponce(HttpServletResponse resp, String content)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().write(content);
+	}
+
+	private void setErrorResponce(HttpServletResponse resp, int status,
+			String content) throws IOException {
+		resp.setStatus(status);
+		resp.setContentType("text/plain");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().write(content);
+	}
+
+	private String createContent(List<ArticleHeader> articles) {
+		if (articles.size() == 0) {
+			throw new IllegalArgumentException("size must > 0");
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		if (articles.size() > 1) {
+			for (int i = 0; i < articles.size() - 1; i++) {
+				sb.append(articles.get(i).toString());
+				sb.append(",");
+			}
+		}
+		sb.append(articles.get(articles.size() - 1).toString());
+		sb.append("]");
+		return sb.toString();
+	}
+
+	private void saveContentToCache(Key key, String content) {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Entity entity = new Entity(ATTICLE_HEADERS, key);
+		entity.setProperty("content", new Text(content));
+		entity.setProperty("time", new Date().getTime());
+		datastore.put(entity);
 	}
 	
-	
+	private String getContentFromCache(final Key key) {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Entity headerEntity = null;
+		try {
+			headerEntity = datastore.get(key);
+		} catch (EntityNotFoundException e1) {
+			e1.printStackTrace();
+			return null;
+		}
+		// check entity ttl
+		if (headerEntity.hasProperty("time")) {
+			long time = (Long) headerEntity.getProperty("time");
+			long liveSec = (new Date().getTime() - time) * 1000;
+			if (liveSec < 60 * 5) { // ttl = 5 min
+				((Text) headerEntity.getProperty("content")).getValue();
+			}
+		}
+		return null;
+	}
+
+	private byte[] getContentFromNet(final String url) throws IOException {
+		URL myURL = new URL(url);
+		URLConnection con = myURL.openConnection();
+		con.setRequestProperty("Accept-Charset", "UTF-8");
+		con.connect();
+		con.setConnectTimeout(8 * 1000);
+		return readByParts(con.getInputStream());
+	}
+
 	private byte[] readByParts(InputStream is) throws IOException {
 		byte[] data;
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -66,17 +134,14 @@ public class AtticleHeadersServlet extends HttpServlet {
 		int readBlockSize = 4048;
 		byte[] readBlockBuffer = new byte[readBlockSize];
 		int actual = 0;
-		while ( (actual != -1)) {
+		while ((actual != -1)) {
 			actual = is.read(readBlockBuffer, 0, readBlockSize);
 			if (actual > 0) {
 				dos.write(readBlockBuffer, 0, actual);
 			}
 		}
-        data = baos.toByteArray();
+		data = baos.toByteArray();
 		return data;
-    }	
-	
-	    
 	}
-	
-	
+
+}
